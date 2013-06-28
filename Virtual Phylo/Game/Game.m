905264c6b620d669c8c0578f021eb67fa7559ed3
Brandon Wong <bjwong@sfu.ca>
2013-06-28 14:54:15 -0700

    -Storyboard, +Game

--- Virtual Phylo/Game/Game.m
+++ Virtual Phylo/Game/Game.m
@@ -2,22 +2,12 @@
 //  Game.m
 //  Virtual Phylo
 //
-//  Created by Suban K on 2013-06-26.
+//  Created by Suban K on 2013-06-28.
 //  Copyright (c) 2013 Group_12. All rights reserved.
 //
 
 #import "Game.h"
 
 @implementation Game
-{
-    @private
-    id game_PlayerA;
-    id game_playerB;
-    id game_playedCardsA; // the cards player A has played on the playing field
-    id game_playedCardsB; // the cards player B has played on the playing field
-    id game_PlayerBarA; // where Player A's hand, draw and discard piles reside
-    id game_PlayerBarB; // where Player B's hand, draw and discard piles reside
-    int game_turnCount;
-    
-}
+
 @end
